b37603ffc429e33092ea5a49bdf343fcf7c42461
Begemotina <Begemotina@nightmail.ru>
2019-11-29 01:59:45 +0300

    5-задание поправил текст

--- lesson_3/tusk_5.py
+++ lesson_3/tusk_5.py
@@ -8,3 +8,30 @@
  то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 
 """
+def numbers_f(number):
+
+    if number.isdigit():
+        number = float(number)
+        return number
+    else:
+        if number[0] == "-":
+            number = float(number)
+            return number
+        else:
+            return 0   # если это не q и не число то будет возвращать 0
+# def_sum()2
+run = True
+result = 0
+while run:
+    numbers = input("Введите числа через пробел или q что бы закончить \n ").split()
+    for number in numbers:
+        if number == "q" or number == "Q":
+            run = False
+            break
+        else:
+            data = numbers_f(number)
+            result += data
+    print(f"Результат: {result}")
+
+print(f"окончательный итог: {result}")
+
