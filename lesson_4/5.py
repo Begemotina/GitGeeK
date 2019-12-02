1c5ee19b7455f2352c6edeb99a85691f72acace0
Begemotina <Begemotina@nightmail.ru>
2019-12-03 02:00:12 +0300

    5 - готово

--- lesson_4/5.py
+++ lesson_4/5.py
@@ -4,3 +4,16 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать функцию reduce().
 '''
+
+from functools import reduce
+
+def my_sum_function(ferst_el, el ):
+    '''
+    :param ferst_el: предидущий элемент
+    :param el:  текущий элемент
+    :return: сумма двух элементов
+    '''
+    return ferst_el * el
+
+print(reduce(my_sum_function, [el for el in range(100, 1001) if el % 2 == 0]))
+
