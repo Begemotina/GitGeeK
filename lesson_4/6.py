6bf550738c8efa8fb115fc609306f20ef17bfa92
Begemotina <Begemotina@nightmail.ru>
2019-12-03 02:22:25 +0300

    6- г0тово

--- lesson_4/6.py
+++ lesson_4/6.py
@@ -5,3 +5,21 @@
 
 Подсказка: использовать функцию count() и cycle() модуля itertools.
 '''
+from itertools import count, cycle
+
+# ACHTUNG - беконечный цикл 1!
+
+# for el in count(int(input('Введите стартовое число '))):
+#     print(el)
+
+# ACHTUNG - беконечный цикл 2!
+
+i = 0
+
+list = [el for el in range(0, i)]
+print(list)
+
+list =["ABC", True, 4**i,[1, 2, 3, 4]] # 4**i всегда равен 1
+for someting in cycle(list):
+    i += 1
+    print(someting)
