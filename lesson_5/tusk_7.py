3c03beafce5cbd1b3a9d0bdac3594a3b0ec29919
Begemotina <Begemotina@nightmail.ru>
2019-12-09 21:31:28 +0300

    5-е задание готово

--- lesson_5/tusk_7.py
+++ lesson_5/tusk_7.py
@@ -11,3 +11,34 @@
 
 Подсказка: использовать менеджеры контекста.
 '''
+
+import json
+#  подготовка
+profit = {}
+profit_average_dict = {}
+prof = 0
+prof_aver = 0
+i = 0
+# получаем данные. считам среднюю и делае
+with open('tusk_7.txt', 'r', encoding="UTF-8") as my_file:
+    for line in my_file:
+        name, type, earning, loss = line.split()
+        profit[name] = int(earning) - int(loss)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {round(prof_aver, 2)}')
+    else:
+        print(f'Прибыль средняя - нет такой. Все в минусе')
+    profit_average_dict = {'средняя прибыль': round(prof_aver, 2)}
+    profit.update(profit_average_dict)
+    print(f'Прибыль каждой компании - {profit}')
+
+# записываем файлик.
+with open('tusk_7.json', 'w', encoding="UTF-8") as write_js:
+    json.dump(profit, write_js)
+
+    js_string = json.dumps(profit)
+    print(f'Создан файл с расширением json.')
