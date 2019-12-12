e120a47a769bb8a631f3eaaafbf728b943369eb6
Begemotina <Begemotina@nightmail.ru>
2019-12-13 02:48:36 +0300

    4-е задание готово

--- lesson_6/tusk_4.py
+++ lesson_6/tusk_4.py
@@ -1,50 +1,98 @@
 '''
-4. Реализовать класс Stationery (канцелярская принадлежность).
-Определить в нем атрибут title (название) и метод draw (отрисовка).
-Метод выводит сообщение “Запуск отрисовки.”
-Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер).
-В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен выводить уникальное сообщение.
-Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты:
+speed, color, name, is_police (булево).
+А также методы: go, stop, turn(direction), которые должны сообщать,
+что машина поехала, остановилась, повернула (куда).
+Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля.
+ Для классов TownCar и WorkCar переопределите метод show_speed.
+ При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+Создайте экземпляры классов, передайте значения атрибутов.
+Выполните доступ к атрибутам, выведите результат.
+Выполните вызов методов и также покажите результат.
 '''
 
 
-class Stationary:
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
 
-    def __init__(self, title):
-        self.title = title
+    def go(self):
+        print(f"машина {self.name} поехала прямо")
 
-    def draw(self):
-        print(f'Запуск отрисовки {self.title}')
+    def stop(self):
+        print(f"машина {self.name} остановилась")
 
+    def turn(self, direction):
+        print(f"машина {self.name} повернула на {direction}")
 
-class Pen(Stationary):
-    def __init__(self, title):
-        super().__init__(title)
-
-    def draw(self):
-        print(f'Запуск отрисовки {self.title}')
-
-class Pencil(Stationary):
-    def __init__(self, title):
-        super().__init__(title)
-
-    def draw(self):
-        print(f'Запуск отрисовки {self.title}')
+    def show_speed(self):
+        print(f'скорость {self.speed}')
 
 
-class Handle(Stationary):
-    def __init__(self, title):
-        super().__init__(title)
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
 
-    def draw(self):
-        print(f'Запуск отрисовки {self.title}')
+    def show_speed(self):
+        if self.speed <= 60:
+            print(f'скорость {self.speed}')
+        else:
+            print(f'!!!ACHTUNG!!!! скорость {self.speed} !!ACHTUNG!!!!')
 
 
-a = Pen("Ручка")
-a.draw()
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
 
-b = Pencil("Карандаш")
-b.draw()
 
-c = Handle("Маркер")
-c.draw()
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        if self.speed <= 40:
+            print(f'скорость {self.speed}')
+        else:
+            print(f'!!!ACHTUNG!!!! скорость {self.speed} !!ACHTUNG!!!!')
+
+
+# я не знаю чего с ней делать наверно это для будущий разработки
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+if __name__ == '__main__':
+    # в скобках будет вот так, потому-что я замучался смотреть порядок
+    bug = TownCar(speed=30, color="Бежевый", name="bug", is_police=False)
+    cat = WorkCar(speed=60, color="зеленый", name="СAT", is_police=False)
+    bugatti_chiron = PoliceCar(speed=30, color="Кораловый", name="Bugatti Chiron", is_police=True)
+    grand_cherokee = TownCar(speed=90, color="Черный", name="Большой Индеец", is_police=False)
+    dog = WorkCar(speed=130, color="Фиолетовый", name="DOG", is_police=True)
+
+    # доступ к атрибутам, выведите результат.
+    # наверно не самый хороший способ делать вот такой кортеж, но все же у меня он будет
+    for i in (bug, cat, grand_cherokee, dog,):
+        print(i.speed)
+        print(i.color)
+        print(i.name)
+        print(i.is_police)
+        if i != dog:
+            print("\nдругая машина")
+
+    # Выполните вызов методов и также покажите результат.
+    for i in (bug, cat, grand_cherokee, dog,):
+        i.go()
+        i.turn("право")
+        i.show_speed()
+        i.go()
+        i.turn("лево")
+        i.go()
+        i.stop()
+        if i != dog:
+            print("\nдругая машина")
+
