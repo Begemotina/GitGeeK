77a1f5eec39804d8c66c2217417c4e787d4e2ed8
Begemotina <Begemotina@nightmail.ru>
2019-12-13 00:45:27 +0300

    3-е задание готово

--- lesson_6/tusk_3.py
+++ lesson_6/tusk_3.py
@@ -1,4 +1,36 @@
 '''
-3. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево). А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar переопределите метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
-Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
+3. Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность),
+income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+оклад и премия, например, {"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+ Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+ проверить значения атрибутов, вызвать методы экземпляров).
 '''
+
+
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        print(f'{self.name} {self.surname}')
+
+    def get_total_income(self):
+        print(self._income.get('wage') + self._income.get('bonus'))
+
+
+a = Position("Лектор", "Канибал", "Людоед", 1000, 2000)
+a.get_full_name()
+a.get_total_income()
